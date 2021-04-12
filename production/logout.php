49701cd013032152dc8467215612a676750d9e30
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 12:58:58 +0530

    logout

---
+++ production/logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+ 	unset($_SESSION);
+ 	session_unset();
+	session_destroy();
+  echo '<script type="text/javascript">';
+ echo 'window.location.href="login.php";';
+ echo '</script>';
+	exit;
+
+?>
