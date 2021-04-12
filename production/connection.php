c8bf514df617a461d362b52a2798486de2a91de8
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 11:27:16 +0530

    connection

---
+++ production/connection.php
@@ -0,0 +1,27 @@
+<?php
+ $servername = "localhost";
+ $username = "u514971509_root";
+ $password = "Gowthamhm001@";
+ $dbname="u514971509_qrcode";
+
+// $servername ="localhost";
+// $username ="root";
+// $password ="";
+// $dbname ="qrcode";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  ?>
+<script type='text/javascript' charset='utf-8'>
+alert("Please Contact Admin with Error Message");
+window.location.replace('login.php');
+</script>
+<?php
+}else{
+echo "Connected successfully";
+}
+?>
