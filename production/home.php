1e082fa6aebab2fc069d45f01281a0d5892e1584
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 13:22:34 +0530

    nav

--- production/home.php
+++ production/home.php
@@ -66,57 +66,12 @@ include 'error.php';
                 <h3>General</h3>
                 <ul class="nav side-menu">
                   <li><a href="home.php" ><i class="fa fa-home"></i> Home </a>
-                    // <ul class="nav child_menu">
-                    //   <li><a href="index.html">Dashboard</a></li>
-                    //   <li><a href="index2.html">Dashboard2</a></li>
-                    //   <li><a href="index3.html">Dashboard3</a></li>
-                    // </ul>
                   </li>
                   <li><a href="instancesms.php"><i class="fa fa-sms"></i> Instance SMS </a>
-                    // <ul class="nav child_menu">
-                    //   <li><a href="form.html">General Form</a></li>
-                    //   <li><a href="form_advanced.html">Advanced Components</a></li>
-                    //   <li><a href="form_validation.html">Form Validation</a></li>
-                    //   <li><a href="form_wizards.html">Form Wizard</a></li>
-                    //   <li><a href="form_upload.html">Form Upload</a></li>
-                    //   <li><a href="form_buttons.html">Form Buttons</a></li>
-                    // </ul>
                   </li>
-                  <li><a><i class="fa fa-"></i> Google Sheets </a>
-                    // <ul class="nav child_menu">
-                    //   <li><a href="general_elements.html">General Elements</a></li>
-                    //   <li><a href="media_gallery.html">Media Gallery</a></li>
-                    //   <li><a href="typography.html">Typography</a></li>
-                    //   <li><a href="icons.html">Icons</a></li>
-                    //   <li><a href="glyphicons.html">Glyphicons</a></li>
-                    //   <li><a href="widgets.html">Widgets</a></li>
-                    //   <li><a href="invoice.html">Invoice</a></li>
-                    //   <li><a href="inbox.html">Inbox</a></li>
-                    //   <li><a href="calendar.html">Calendar</a></li>
-                    // </ul>
+                  <li><a href="spreadsheet.php"><i class="fas fa-file-spreadsheet"></i> Google Sheets</a>
                   </li>
-                  <li><a href="spreadsheet.php"><i class="fas fa-file-spreadsheet"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                  //   <ul class="nav child_menu">
-                  //     <li><a href="tables.html">Tables</a></li>
-                  //     <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                  //   </ul>
-                  // </li>
-                  // <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                  //   <ul class="nav child_menu">
-                  //     <li><a href="chartjs.html">Chart JS</a></li>
-                  //     <li><a href="chartjs2.html">Chart JS2</a></li>
-                  //     <li><a href="morisjs.html">Moris JS</a></li>
-                  //     <li><a href="echarts.html">ECharts</a></li>
-                  //     <li><a href="other_charts.html">Other Charts</a></li>
-                  //   </ul>
-                  </li>
-                  // <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                  //   <ul class="nav child_menu">
-                  //     <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                  //     <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                  //   </ul>
-                  // </li>
-                </ul>
+                  </ul>
               </div>
             </div>
             <!-- /sidebar menu -->
@@ -132,71 +87,6 @@ include 'error.php';
               <ul class=" navbar-right">
                 <li class="nav-item open" style="padding-left: 15px;">
                   <a class="dropdown-item"  href="logout.php"><i class="fa fa-sign-out"></i> Log Out</a>
-                </li>
-
-                <li role="presentation" class="nav-item dropdown open">
-                  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-envelope-o"></i>
-                    <span class="badge bg-green">6</span>
-                  </a>
-                  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <div class="text-center">
-                        <a class="dropdown-item">
-                          <strong>See All Alerts</strong>
-                          <i class="fa fa-angle-right"></i>
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
                 </li>
               </ul>
             </nav>
