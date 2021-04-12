0658aa26e0a79f627ab607beab9dabc1c6337309
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 15:33:32 +0530

    select

--- production/instancesms.php
+++ production/instancesms.php
@@ -260,6 +260,12 @@ jQuery.fn.extend({
         this.val(arr);
     },
 });</script>
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<script>
+    $(document).ready(function () {
+        $("#test").CreateMultiCheckBox({ width: '230px', defaultText : 'Select Below', height:'250px' });
+    });
+</script>
   </body>
 
 </html>
