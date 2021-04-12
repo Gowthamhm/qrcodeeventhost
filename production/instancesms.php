f5c5ce56d5479d8aa4d652b263b925ae8054b284
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 15:08:28 +0530

    form

--- production/instancesms.php
+++ production/instancesms.php
@@ -96,85 +96,13 @@ include 'error.php';
         <div class="row">
   						<div class="col-md-12 col-sm-12 ">
   							<div class="x_panel">
-  								<div class="x_title">
-  									<h2>Form Design <small>different form elements</small></h2>
-  									<ul class="nav navbar-right panel_toolbox">
-  										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-  										</li>
-  										<li class="dropdown">
-  											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-  											<ul class="dropdown-menu" role="menu">
-  												<li><a class="dropdown-item" href="#">Settings 1</a>
-  												</li>
-  												<li><a class="dropdown-item" href="#">Settings 2</a>
-  												</li>
-  											</ul>
-  										</li>
-  										<li><a class="close-link"><i class="fa fa-close"></i></a>
-  										</li>
-  									</ul>
-  									<div class="clearfix"></div>
-  								</div>
   								<div class="x_content">
   									<br />
   									<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-
-  										<div class="item form-group">
-  											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">First Name <span class="required">*</span>
-  											</label>
-  											<div class="col-md-6 col-sm-6 ">
-  												<input type="text" id="first-name" required="required" class="form-control ">
-  											</div>
-  										</div>
-  										<div class="item form-group">
-  											<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Last Name <span class="required">*</span>
-  											</label>
-  											<div class="col-md-6 col-sm-6 ">
-  												<input type="text" id="last-name" name="last-name" required="required" class="form-control">
-  											</div>
-  										</div>
-  										<div class="item form-group">
-  											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Middle Name / Initial</label>
-  											<div class="col-md-6 col-sm-6 ">
-  												<input id="middle-name" class="form-control" type="text" name="middle-name">
-  											</div>
-  										</div>
-  										<div class="item form-group">
-  											<label class="col-form-label col-md-3 col-sm-3 label-align">Gender</label>
-  											<div class="col-md-6 col-sm-6 ">
-  												<div id="gender" class="btn-group" data-toggle="buttons">
-  													<label class="btn btn-secondary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-  														<input type="radio" name="gender" value="male" class="join-btn"> &nbsp; Male &nbsp;
-  													</label>
-  													<label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-  														<input type="radio" name="gender" value="female" class="join-btn"> Female
-  													</label>
-  												</div>
-  											</div>
-  										</div>
-  										<div class="item form-group">
-  											<label class="col-form-label col-md-3 col-sm-3 label-align">Date Of Birth <span class="required">*</span>
-  											</label>
-  											<div class="col-md-6 col-sm-6 ">
-  												<input id="birthday" class="date-picker form-control" placeholder="dd-mm-yyyy" type="text" required="required" type="text" onfocus="this.type='date'" onmouseover="this.type='date'" onclick="this.type='date'" onblur="this.type='text'" onmouseout="timeFunctionLong(this)">
-  												<script>
-  													function timeFunctionLong(input) {
-  														setTimeout(function() {
-  															input.type = 'text';
-  														}, 60000);
-  													}
-  												</script>
-  											</div>
-  										</div>
-  										<div class="ln_solid"></div>
-  										<div class="item form-group">
-  											<div class="col-md-6 col-sm-6 offset-md-3">
-  												<button class="btn btn-primary" type="button">Cancel</button>
-  												<button class="btn btn-primary" type="reset">Reset</button>
-  												<button type="submit" class="btn btn-success">Submit</button>
-  											</div>
-  										</div>
-
+                    <div class="col-md-6 col-sm-6  form-group has-feedback">
+  <input type="tel" class="form-control" id="inputSuccess5" placeholder="Phone">
+  <span class="fa fa-phone form-control-feedback right" aria-hidden="true"></span>
+</div>
   									</form>
   								</div>
   							</div>
