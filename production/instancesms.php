6f540d6da86c9bafec6391e6a050ad3ea62df03c
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 22:26:05 +0530

    check box

--- production/instancesms.php
+++ production/instancesms.php
@@ -112,36 +112,9 @@ include 'error.php';
                     <div class="col-md-9 col-sm-9 ">
                       <select class="select2_single form-control" tabindex="-1">
                       <option></option>
-<option><lable><input type="checkbox" value=" "></lable></option>
-<option><lable><input type="checkbox" value=" "></lable></option>
-<option><lable><input type="checkbox" value=" "></lable></option>
-<option value="AK">Alaska</option>
-                        <input type="checkbox" value=" "><option value="HI">Hawaii</option>
-                    <input type="checkbox" value=" ">    <option value="CA">California</option>
-                    <input type="checkbox" value=" ">    <option value="NV">Nevada</option>
-                        <option value="OR">Oregon</option>
-                        <option value="WA">Washington</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="CO">Colorado</option>
-                        <option value="ID">Idaho</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="UT">Utah</option>
-                        <option value="WY">Wyoming</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TX">Texas</option>
+<lable><input type="checkbox" value=" "></lable>
+<lable><input type="checkbox" value=" "></lable>
+<lable><input type="checkbox" value=" "></lable>
                       </select>
                     </div>
                   </div>
