a0fda60063c7bdd708fe91ffe7eb25de97563da9
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 11:20:15 +0530

    icon

--- production/login.php
+++ production/login.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel="shortcut icon" href="logo.ico">
+<link rel="shortcut icon" href="images/logo.ico">
 <link href="css/login.css" rel="stylesheet" type="text/css">
   </head>
   <body class="my-login-page">
