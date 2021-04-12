f1311ba7af9066f607862fdecbff3b8d46ad34dd
Gowthamhm <gowthamhm001@gmail.com>
2021-04-12 14:21:05 +0530

    script

--- production/home.php
+++ production/home.php
@@ -168,13 +168,13 @@ include 'error.php';
 
   </body>
   <script type="text/javascript" charset="utf-8">
-  $('#collapseOne').on('shown.bs.collapse', function () {
-    $(".fa").removeClass("fa-folder-o").addClass("fa-folder-open-o");
-});
-
-$('#collapseOne').on('hidden.bs.collapse', function () {
-    $(".fa").removeClass("fa-folder-open-o").addClass("fa-folder-o");
-});
+//   $('#collapseOne').on('shown.bs.collapse', function () {
+//     $(".fa").removeClass("fa-folder-o").addClass("fa-folder-open-o");
+// });
+//
+// $('#collapseOne').on('hidden.bs.collapse', function () {
+//     $(".fa").removeClass("fa-folder-open-o").addClass("fa-folder-o");
+// });
 
 function showf() {
     var x = document.getElementById("collapseOne");
