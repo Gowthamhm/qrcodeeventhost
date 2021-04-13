816e8bc0e312af85f93790972b839eb90bbf24ba
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 16:48:04 +0530

    responsive

--- production/instancesms.php
+++ production/instancesms.php
@@ -146,7 +146,7 @@ include 'error.php';
                   <div class="x_content">
                       <div class="row">
                           <div class="col-sm-12">
-                    <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
+                <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                       <thead>
                         <tr>
                           <th>Sl No.</th>
