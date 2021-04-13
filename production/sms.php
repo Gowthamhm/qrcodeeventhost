c93c740fc33fe0de773ccd8a728a1564eb06b373
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 22:31:09 +0530

    js

--- production/sms.php
+++ production/sms.php
@@ -147,7 +147,7 @@ function showeditor() {
     <!-- Bootstrap -->
    <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- <script src="../vendors/fastclick/lib/fastclick.js"></script> -->
     <!-- NProgress -->
     <!-- <script src="../vendors/nprogress/nprogress.js"></script> -->
     <!-- iCheck -->
@@ -170,7 +170,7 @@ function showeditor() {
     <script src="../vendors/pdfmake/build/vfs_fonts.js"></script> -->
 
     <!-- Custom Theme Scripts -->
-    <script src="../build/js/custom.min.js"></script>
+    <!-- <script src="../build/js/custom.min.js"></script> -->
 
   </body>
 </html>
