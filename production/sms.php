3fc4bc8ee9858c78e20faf738a5d06f24c8e78d8
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 17:22:10 +0530

    submit

--- production/sms.php
+++ production/sms.php
@@ -1,13 +1,8 @@
 <?php
 if(isset($_POST['send'])){
-echo $_POST['table_records'];
-foreach ($name as $table_records){
-    echo $table_records."<br />";
+  echo $_POST['number'];
 }
-}else{
-  echo $_POST['table_records'];
-    echo $_POST['table_records']['0'];
-  echo 'f**k';
+else{
+  echo $_POST['number'];
 }
-
 ?>
