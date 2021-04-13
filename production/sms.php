e00d6e1d9595ac55770d1aa654cba4fcf26a330c
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 07:42:23 +0530

    lets check

--- production/sms.php
+++ production/sms.php
@@ -1,8 +1,12 @@
 <?php
 if(isset($_POST['send'])){
 echo $_POST['table_records'];
+  echo $_POST['table_records']['0'];
+    echo $_POST['table_records']['1'];
+      echo $_POST['table_records']['2'];
 }else{
   echo $_POST['table_records'];
+    echo $_POST['table_records']['0'];
   echo 'f**k';
 }
 
