486e5e6ebe767f6e8db9ce2a6806a85dfba105b8
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 22:29:41 +0530

    sms table

--- production/sms.php
+++ production/sms.php
@@ -149,11 +149,11 @@ function showeditor() {
     <!-- FastClick -->
     <script src="../vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- <script src="../vendors/nprogress/nprogress.js"></script> -->
     <!-- iCheck -->
-    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <!-- <script src="../vendors/iCheck/icheck.min.js"></script> -->
     <!-- Datatables -->
-    <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <!-- <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
     <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
     <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
@@ -167,7 +167,7 @@ function showeditor() {
     <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
     <script src="../vendors/jszip/dist/jszip.min.js"></script>
     <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
+    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script> -->
 
     <!-- Custom Theme Scripts -->
     <script src="../build/js/custom.min.js"></script>
