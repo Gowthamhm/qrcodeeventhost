2a79b10cf805eb538e9bd2f951f52fa04f0396d9
Gowthamhm <gowthamhm001@gmail.com>
2021-04-13 20:27:44 +0530

    text

--- production/sms.php
+++ production/sms.php
@@ -21,6 +21,10 @@ include 'error.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="images/logo.ico" type="image/ico" />
+  <script src="ckeditor.js"></script>
+  <script src="js/sample.js"></script>
+  <link rel="stylesheet" href="css/samples.css">
+  <link rel="stylesheet" href="toolbarconfigurator/lib/codemirror/neo.css">
 
     <title>Message</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
@@ -99,113 +103,16 @@ include 'error.php';
         </div>
         <!-- /top navigation -->
         <!-- page content -->
-  			<div class="right_col" role="main">
-  				<div class="">
-  					<div class="clearfix"></div>
-  					<div class="col-md-12 col-sm-12 ">
-  						<div class="x_panel">
-  							<div class="x_title">
-  								<h2>Enter the Message<small>Sessions</small></h2>
-  								<div class="clearfix"></div>
-  							</div>
-                <div class="">
-                  <form class="" action="send_sms.php" method="post">
-                  <input type="hidden" name="number" value="<?php echo $numbers;?>">
-                </div>
-  							<div class="x_content">
-  								<div id="alerts"></div>
-  								<div class="btn-toolbar editor" data-role="editor-toolbar" data-target="#editor-one">
-  									<div class="btn-group">
-  										<a class="btn dropdown-toggle" data-toggle="dropdown" title="Font"><i class="fa fa-font"></i><b class="caret"></b></a>
-  										<ul class="dropdown-menu">
-  										</ul>
-  									</div>
+        <div id="editor">
 
-  									<div class="btn-group">
-  										<a class="btn dropdown-toggle" data-toggle="dropdown" title="Font Size"><i class="fa fa-text-height"></i>&nbsp;<b class="caret"></b></a>
-  										<ul class="dropdown-menu">
-  											<li>
-  												<a data-edit="fontSize 5">
-  													<p style="font-size:17px">Huge</p>
-  												</a>
-  											</li>
-  											<li>
-  												<a data-edit="fontSize 3">
-  													<p style="font-size:14px">Normal</p>
-  												</a>
-  											</li>
-  											<li>
-  												<a data-edit="fontSize 1">
-  													<p style="font-size:11px">Small</p>
-  												</a>
-  											</li>
-  										</ul>
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn" data-edit="bold" title="Bold (Ctrl/Cmd+B)"><i class="fa fa-bold"></i></a>
-  										<a class="btn" data-edit="italic" title="Italic (Ctrl/Cmd+I)"><i class="fa fa-italic"></i></a>
-  										<a class="btn" data-edit="strikethrough" title="Strikethrough"><i class="fa fa-strikethrough"></i></a>
-  										<a class="btn" data-edit="underline" title="Underline (Ctrl/Cmd+U)"><i class="fa fa-underline"></i></a>
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn" data-edit="insertunorderedlist" title="Bullet list"><i class="fa fa-list-ul"></i></a>
-  										<a class="btn" data-edit="insertorderedlist" title="Number list"><i class="fa fa-list-ol"></i></a>
-  										<a class="btn" data-edit="outdent" title="Reduce indent (Shift+Tab)"><i class="fa fa-dedent"></i></a>
-  										<a class="btn" data-edit="indent" title="Indent (Tab)"><i class="fa fa-indent"></i></a>
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn" data-edit="justifyleft" title="Align Left (Ctrl/Cmd+L)"><i class="fa fa-align-left"></i></a>
-  										<a class="btn" data-edit="justifycenter" title="Center (Ctrl/Cmd+E)"><i class="fa fa-align-center"></i></a>
-  										<a class="btn" data-edit="justifyright" title="Align Right (Ctrl/Cmd+R)"><i class="fa fa-align-right"></i></a>
-  										<a class="btn" data-edit="justifyfull" title="Justify (Ctrl/Cmd+J)"><i class="fa fa-align-justify"></i></a>
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn dropdown-toggle" data-toggle="dropdown" title="Hyperlink"><i class="fa fa-link"></i></a>
-  										<div class="dropdown-menu input-append">
-  											<input class="span2" placeholder="URL" type="text" data-edit="createLink" />
-  											<button class="btn" type="button">Add</button>
-  										</div>
-  										<a class="btn" data-edit="unlink" title="Remove Hyperlink"><i class="fa fa-cut"></i></a>
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn" title="Insert picture (or just drag & drop)" id="pictureBtn"><i class="fa fa-picture-o"></i></a>
-  										<input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" />
-  									</div>
-
-  									<div class="btn-group">
-  										<a class="btn" data-edit="undo" title="Undo (Ctrl/Cmd+Z)"><i class="fa fa-undo"></i></a>
-  										<a class="btn" data-edit="redo" title="Redo (Ctrl/Cmd+Y)"><i class="fa fa-repeat"></i></a>
-  									</div>
-  								</div>
-
-  								<div id="editor-one" class="editor-wrapper"></div>
-
-  								<textarea name="descr" id="descr" style="display:none;"></textarea>
-
-  								<br />
-
-  								<div class="ln_solid"></div>
-
-  							</div>
-  						</div>
-              <input type="submit" name="sendsms" value="Send Message">
-  					</div>
-  </form>
-
-  				</div>
-  			</div>
-  			<!-- /page content -->
+        				</div>
+      	<!-- /page content -->
 
       </div>
     </div>
-<script type="text/javascript">
-
-</script>
+    <script>
+    	initSample();
+    </script>
     <!-- jQuery -->
     <script src="../vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
