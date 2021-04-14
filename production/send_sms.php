afa482871c3b0c3b0ef1c1d7b0e40b26bfdc65bf
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 07:14:28 +0530

    test

--- production/send_sms.php
+++ production/send_sms.php
@@ -1,15 +1,14 @@
 <?php
-if(isset($_POST['sendsms'])){
-$text= $_POST['text'];
-$number = $_POST['numbers'];
+// if(isset($_POST['sendsms'])){
+// $text= $_POST['text'];
+// $number = $_POST['numbers'];
 
 $service_plan_id = "78125b9858494c72894913f48031923d";
 $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
 
 $send_from = "+447537454577";
-$recipient_phone_numbers = $number; //May be several, separate with a comma `,`.
-$message = $text;
-// "This test message will be sent to {$recipient_phone_numbers} from {$assigned_phone_number}";
+$recipient_phone_numbers = ",9483621844,8095642067" ; //May be several, separate with a comma `,`.
+$message = "This test message will be sent to {$recipient_phone_numbers} from {$assigned_phone_number}";
 
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
@@ -51,5 +50,5 @@ echo $message;
   curl_close($ch);
 }
 }
-}
+// }
 ?>
