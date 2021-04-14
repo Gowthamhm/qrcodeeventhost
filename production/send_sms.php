30abca415c758c04ee82f7b52e067639aa3ab0aa
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 06:43:55 +0530

    sdfg

--- production/send_sms.php
+++ production/send_sms.php
@@ -19,7 +19,7 @@ $send_from = "+447537454577";
 $recipient_phone_numbers = $number; //May be several, separate with a comma `,`.
 $message = $text;
 // "This test message will be sent to {$recipient_phone_numbers} from ";
-// echo "$message";
+echo "$message";
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
@@ -35,7 +35,7 @@ if (strlen($number)==10) {
   $content = [
     'to' => $number,
     'from' => $send_from,
-    'body' => $text
+    'body' => $message
   ];
 
   $data = json_encode($content);
