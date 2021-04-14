5002f8b35a85ef3e54bf7870426ef4552409b653
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 14:29:40 +0530

    download

--- production/home.php
+++ production/home.php
@@ -69,6 +69,8 @@ include 'error.php';
                   </li>
                   <li><a href="sendQrcode.php"><i class="fa fa-comments-o"></i> Share QrCode </a>
                   </li>
+                  <li><a href="download.php"><i class="fa fa-download"></i> Download</a>
+                  </li>
                   </ul>
               </div>
             </div>
