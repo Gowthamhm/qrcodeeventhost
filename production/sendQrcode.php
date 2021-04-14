1f733f7c368102c099c0c9643041cd913ee203e0
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 14:21:50 +0530

    header

--- production/sendQrcode.php
+++ production/sendQrcode.php
@@ -101,7 +101,7 @@ include 'error.php';
               <div class="col-md-12 col-sm-12 ">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Instance message to numbers</small></h2>
+                    <h2>Share QrCode</h2>
 
                     <div class="clearfix"></div>
                   </div>
