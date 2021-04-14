ffa0b2b371d4da0ed242a937c4373d5f52d3bdc0
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 07:16:51 +0530

    sms

--- production/send_sms.php
+++ production/send_sms.php
@@ -6,10 +6,10 @@
 $service_plan_id = "78125b9858494c72894913f48031923d";
 $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
 
-$send_from = "447537454577";
+$assigned_phone_number = "+447537454577";
 $recipient_phone_numbers = ",9483621844,8095642067" ; //May be several, separate with a comma `,`.
+// $message =$text;
 $message = "This test message will be sent to {$recipient_phone_numbers} from {$assigned_phone_number}";
-
 // Check recipient_phone_numbers for multiple numbers and make it an array.
 if(stristr($recipient_phone_numbers, ',')){
   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
@@ -25,7 +25,7 @@ echo $message;
   // Set necessary fields to be JSON encoded
   $content = [
     'to' => $number,
-    'from' => $send_from,
+    'from' => $assigned_phone_number,
     'body' => $message
   ];
 
