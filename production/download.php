44e66ee84f59e9ea1ff5684b56d01ca8222c60e3
Gowthamhm <gowthamhm001@gmail.com>
2021-04-14 17:35:13 +0530

    download

---
+++ production/download.php
@@ -0,0 +1,26 @@
+<?php
+include 'connection.php';
+include 'session.php';
+include 'error.php';
+$filename	=	'qrcodes.zip';
+	$fileQry	=	$conn->query('SELECT * FROM `qrcode`');
+
+	$zip = new ZipArchive;
+	if ($zip->open($filename,  ZipArchive::CREATE)){
+		while($row	=	$fileQry->fetch_assoc()){
+			$zip->addFile(getcwd().'./'.'resource/'.$row['folder_name'].'/', $row['infilename']);
+		}
+
+		$zip->close();
+
+		header("Content-type: application/zip");
+		header("Content-Disposition: attachment; filename=$filename");
+		header("Content-length: " . filesize($filename));
+		header("Pragma: no-cache");
+		header("Expires: 0");
+		readfile("$filename");
+		unlink($filename);
+	}else{
+	   echo 'Failed!';
+	}
+?>
