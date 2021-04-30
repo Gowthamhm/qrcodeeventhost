daf0a7a037d64867348ce7ce55786378fe133df7
Gowthamhm <gowthamhm001@gmail.com>
2021-04-30 21:04:04 +0530

    qrcode scanner

---
+++ production/qrcodereader.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+
+<form class="" method="post">
+<input type="text" name="qrcode" value="">
+<input type="submit" name="submit" value="Submit">
+</form>
+
+  </body>
+</html>
