9d10d3a5a8f0b2608ee43d4698f4c471585ea3b6
Gowthamhm <gowthamhm001@gmail.com>
2021-05-07 21:45:30 +0530

    status update

--- production/scannedTextDisplay.php
+++ production/scannedTextDisplay.php
@@ -195,7 +195,7 @@ curl_close($ch);
             // echo $number.$text.$update_status;
         }else if($row['status'] == 99){
           ?><script type="text/javascript" charset="utf-8">
-         alert("In Qrcode Is Already Scanned, Please CHeck Once");
+         alert("In Qrcode Is Already Scanned, Please Check Once");
          window.location.replace('qrcodereader.php');
          </script>
          <?php
@@ -212,7 +212,40 @@ curl_close($ch);
 $pattern = "/out.png/i";
   if (preg_match($pattern, $str_arr[5])) {
   $selectdata = "SELECT * FROM `qrcode` where folder_name ='".$str_arr[0]."'and outfilename ='".$str_arr[5]."' and number='".$str_arr[6]."'";
-
+  $result = $conn->query($selectdata);
+  echo $result->num_rows;
+  if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          if($row['status'] == 1){
+            ?><script type="text/javascript" charset="utf-8">
+           alert("Please Check Scanning OutQrCode Before InQrcode");
+           window.location.replace('qrcodereader.php');
+           </script>
+           <?php
+          } else if($row['status'] == 99){
+            $update_status = "UPDATE `qrcode` set status= 999 where folder_name ='".$str_arr[0]."'and infilename ='".$str_arr[5]."' and number='".$str_arr[6]."'";
+            if($conn->query($update_status) === TRUE){
+              ?><script type="text/javascript" charset="utf-8">
+               alert("Status updated Successfully");
+               </script>
+               <?php
+            }else {
+              ?><script type="text/javascript" charset="utf-8">
+              alert("Can't able to Status updated Successfully");
+               window.location.replace('qrcodereader.php');
+               </script>
+               <?php
+            }
+          }
+          else if($row['status'] == 999){
+            ?><script type="text/javascript" charset="utf-8">
+           alert("In and Out Qrcode are Scanned, Varify Once Again");
+           window.location.replace('qrcodereader.php');
+           </script>
+           <?php
+          }
+        }
+      }
 }
  }
  // echo "$selectdata";
