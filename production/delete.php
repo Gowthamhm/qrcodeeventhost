570d331322ce5e99ca42d7ff66de77da3a3efcf9
Gowthamhm <gowthamhm001@gmail.com>
2021-05-08 13:32:29 +0530

    delete select query

--- production/delete.php
+++ production/delete.php
@@ -6,19 +6,31 @@ include 'error.php';
 if(isset($_POST['delete'])){
   // echo $_POST['foldername'];
   $folder_name=$_POST['foldername'];
-  $delete = "DELETE from `qrcode` where folder_name='".$folder_name."';";
-  echo $delete;
-   if($conn->query($delete) === TRUE) {
+  $checkFolder = "SELECT * FROM `folders` WHERE folder_name='".$folder_name."'";
+  // echo $checkFolder;
+  $checkResult = $conn->query($checkFolder);
+  if ($checkResult->num_rows > 0) {
+    while($row = $checkResult->fetch_assoc()) {
+      $slno = $row['slno'];
+  $dbcreator = $row['creator'];
+  $dbfoldername = $row['folder_name'];
+  echo $slno.$dbcreator.$dbfoldername;
+  }
+}
+  // $delete = "DELETE from `qrcode` where folder_name='".$folder_name."';";
+  // echo $delete;
+   // if($conn->query($delete) === TRUE) {
      // echo "Deleted data successfully\n";
      // mysql_close($conn);
-     ?> <script type="text/javascript" charset="utf-8">
-      alert("Data Deleted successfully");
-      window.location.replace('home.php');
-      </script>
+     ?>
+      // <script type="text/javascript" charset="utf-8">
+      // alert("Data Deleted successfully");
+      // window.location.replace('home.php');
+      // </script>
       <?php
-   }else {
-     die('Could not delete data: ' . mysql_error());
-   }
+   // }else {
+     // die('Could not delete data: ' . mysql_error());
+   // }
 }else {
 
 }
