792c65aaadc426256b7bf3df8b9845e7d245c3fb
Gowthamhm <gowthamhm001@gmail.com>
2021-05-08 13:20:13 +0530

    delete

--- production/delete.php
+++ production/delete.php
@@ -1,7 +1,25 @@
 <?php
+include 'session.php';
+include 'connection.php';
+include 'error.php';
+
 if(isset($_POST['delete'])){
-  echo $_POST['foldername'];
+  // echo $_POST['foldername'];
+  $delete = "DELETE from `qrcode` where folder_name='". $_POST['foldername']."'";
+  $retval = mysql_query( $delete, $conn );
+
+   if(! $retval ) {
+      die('Could not delete data: ' . mysql_error());
+   }else {
+     echo "Deleted data successfully\n";
+     mysql_close($conn);
+     ?> <script type="text/javascript" charset="utf-8">
+      alert("Data Deleted successfully");
+      window.location.replace('home.php');
+      </script>
+      <?php
+   }
 }else {
-  echo $_POST['delete'];
+
 }
 ?>
