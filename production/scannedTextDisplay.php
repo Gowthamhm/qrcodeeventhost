1c8304da451d272704a89aae6c12d541fa1a31d5
Gowthamhm <gowthamhm001@gmail.com>
2021-05-08 20:41:05 +0530

    display text

--- production/scannedTextDisplay.php
+++ production/scannedTextDisplay.php
@@ -254,9 +254,15 @@ $pattern = "/out.png/i";
           if($row['status'] == 1){
             ?><script type="text/javascript" charset="utf-8">
            alert("Please Check Scanning OutQrCode Before InQrcode");
-           window.location.replace('qrcodereader.php');
+           // window.location.replace('qrcodereader.php');
            </script>
            <?php
+           echo "<script>";
+  echo "setTimeout(function(){";
+    echo "window.location.href = 'qrcodereader.php';";
+      echo " }, 30000);";
+      // echo " }, 30);";
+  echo "</script>";
           } else if($row['status'] == 99){
             $update_status = "UPDATE `qrcode` set status= 999 where folder_name ='".$str_arr[0]."'and outfilename ='".$str_arr[5]."' and number='".$str_arr[6]."'";
             if($conn->query($update_status) === TRUE){
