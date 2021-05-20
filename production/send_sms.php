64ed55860462c36380e3cc9beeba2d7ff62f45b5
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 20:15:26 +0530

    check

--- production/send_sms.php
+++ production/send_sms.php
@@ -28,6 +28,7 @@ for($i=0;$i<count($number);$i++){
   // echo $number[$i];
   $number_to_send =  '+91'.$number[$i];
 // echo $number_to_send;
+if(count($number_to_send) == 13){
   $client->messages->create(
       // Where to send a text message (your cell phone?)
       $number_to_send,
@@ -36,6 +37,7 @@ for($i=0;$i<count($number);$i++){
           'body' => $text
       )
   );
+}
 }
 
 
