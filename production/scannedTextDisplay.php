d8e260a5d0554e9ef39cda50fbe24e0b5f365ffe
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 22:02:21 +0530

    out qrcode

--- production/scannedTextDisplay.php
+++ production/scannedTextDisplay.php
@@ -1,4 +1,8 @@
 <?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
 include 'session.php';
 include 'connection.php';
 include 'error.php';
@@ -155,77 +159,108 @@ include 'error.php';
               echo "</div>";
               $number = "+91" . $row['number'];
               $text = "https://sample-wesite-hosting.online/production" . str_replace(".", '', $row['path']) . "/" . $row['outfilename'];
-              $service_plan_id = "78125b9858494c72894913f48031923d";
-              $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
+              // $service_plan_id = "78125b9858494c72894913f48031923d";
+              // $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
+              //
+              // $send_from = "+447537454577";
 
-              $send_from = "+447537454577";
-              $recipient_phone_numbers = $number; //May be several, separate with a comma `,`.
-              $message = $text;
-              // Check recipient_phone_numbers for multiple numbers and make it an array.
-              if (stristr($recipient_phone_numbers, ',')) {
-                $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
-              } else {
-                $recipient_phone_numbers = [$recipient_phone_numbers];
-              }
+              // Your Account SID and Auth Token from twilio.com/console
+              $account_sid = 'AC11111a46dcd23e4a639e77e6088b32c4';
+              $auth_token = '70afc0450391fadee1586c848756642b';
+            // In production, these should be environment variables. E.g.:
+            // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+
+            // A Twilio number you own with SMS capabilities
+            $twilio_number = "+17204087706";
+            try{
+             $client = new Client($account_sid, $auth_token);
+             $client->messages->create(
+                 // Where to send a text message (your cell phone?)
+                 $number,
+                 array(
+                     'from' => $twilio_number,
+                     'body' => $text
+                 )
+             );
+
+             $update_status = "UPDATE `qrcode` set status= 99 where folder_name ='" . $str_arr[0] . "'and infilename ='" . $str_arr[5] . "' and number='" . $str_arr[6] . "'";
+             if ($conn->query($update_status) === TRUE) {
+               // <script type="text/javascript" charset="utf-8">
+               //  alert("Out QrCode Send Successfully");
+               //   window.location.replace('qrcodereader.php');
+               //   // setTimeout(function(){
+               //   //  window.location.href = 'qrcodereader.php';
+               //   //      }, 3000);
+               //  </script>
+               echo "<script type='text/javascript' charset='utf-8'>";
+               echo "alert('Out QrCode Send Successfully');";
+               // echo "window.location.replace('qrcodereader.php');";
+               echo "setTimeout(function(){ ";
+               echo " window.location.href = 'qrcodereader.php';";
+               echo "    }, 30000);";
+               echo "</script>";
+             } else {
+               echo "<script type='text/javascript' charset='utf-8'>";
+               echo "alert('Out QrCode Send Successfully but Can't able to update in DB Please scan once Again');";
+               // echo "window.location.replace('qrcodereader.php');";
+               echo "setTimeout(function(){ ";
+               echo " window.location.href = 'qrcodereader.php';";
+               echo "    }, 30000);";
+               echo "</script>";
+               // <!-- <script type="text/javascript" charset="utf-8">
+               // alert("Out QrCode Send Successfully but Can't able to update in DB Please scan once Again");
+               //  window.location.replace('qrcodereader.php');
+               //  // setTimeout(function(){
+               //  //  window.location.href = 'qrcodereader.php';
+               //  //      }, 3000);
+               //  </script> -->
+             }
+           }catch (Exception $e){
+             echo "<script type='text/javascript' charset='utf-8'>";
+             echo "alert('Out QrCode Not Send Successfully, Please Scan Once Again');";
+             // echo "window.location.replace('qrcodereader.php');";
+             echo "setTimeout(function(){ ";
+             echo " window.location.href = 'qrcodereader.php';";
+             echo "    }, 30000);";
+             echo "</script>";
+           }
+
+              // $recipient_phone_numbers = $number; //May be several, separate with a comma `,`.
+              // $message = $text;
+              // // Check recipient_phone_numbers for multiple numbers and make it an array.
+              // if (stristr($recipient_phone_numbers, ',')) {
+              //   $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
+              // } else {
+              //   $recipient_phone_numbers = [$recipient_phone_numbers];
+              // }
 
               // Set necessary fields to be JSON encoded
-              $content = [
-                'to' => array_values($recipient_phone_numbers),
-                'from' => $send_from,
-                'body' => $message
-              ];
+              // $content = [
+              //   'to' => array_values($recipient_phone_numbers),
+              //   'from' => $send_from,
+              //   'body' => $message
+              // ];
+              //
+              // $data = json_encode($content);
+              //
+              // $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
+              // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+              // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+              // curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
+              // curl_setopt($ch, CURLOPT_POST, true);
+              // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+              // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+              //
+              // $result = curl_exec($ch);
 
-              $data = json_encode($content);
-
-              $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
-              curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-              curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-              curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
-              curl_setopt($ch, CURLOPT_POST, true);
-              curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-              curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-              $result = curl_exec($ch);
-
-              if (curl_errno($ch)) {
-                echo 'Curl error: ' . curl_error($ch);
-              } else {
+              // if (curl_errno($ch)) {
+              //   echo 'Curl error: ' . curl_error($ch);
+              // } else {
                 // echo $result;
-                $update_status = "UPDATE `qrcode` set status= 99 where folder_name ='" . $str_arr[0] . "'and infilename ='" . $str_arr[5] . "' and number='" . $str_arr[6] . "'";
-                if ($conn->query($update_status) === TRUE) {
-                  // <script type="text/javascript" charset="utf-8">
-                  //  alert("Out QrCode Send Successfully");
-                  //   window.location.replace('qrcodereader.php');
-                  //   // setTimeout(function(){
-                  //   //  window.location.href = 'qrcodereader.php';
-                  //   //      }, 3000);
-                  //  </script>
-                  echo "<script type='text/javascript' charset='utf-8'>";
-                  echo "alert('Out QrCode Send Successfully');";
-                  // echo "window.location.replace('qrcodereader.php');";
-                  echo "setTimeout(function(){ ";
-                  echo " window.location.href = 'qrcodereader.php';";
-                  echo "    }, 30000);";
-                  echo "</script>";
-                } else {
-                  echo "<script type='text/javascript' charset='utf-8'>";
-                  echo "alert('Out QrCode Send Successfully but Can't able to update in DB Please scan once Again');";
-                  // echo "window.location.replace('qrcodereader.php');";
-                  echo "setTimeout(function(){ ";
-                  echo " window.location.href = 'qrcodereader.php';";
-                  echo "    }, 30000);";
-                  echo "</script>";
-                  // <!-- <script type="text/javascript" charset="utf-8">
-                  // alert("Out QrCode Send Successfully but Can't able to update in DB Please scan once Again");
-                  //  window.location.replace('qrcodereader.php');
-                  //  // setTimeout(function(){
-                  //  //  window.location.href = 'qrcodereader.php';
-                  //  //      }, 3000);
-                  //  </script> -->
-                }
-              }
-              curl_close($ch);
+
+              // }
+              // curl_close($ch);
               // echo $number.$text.$update_status;
             } else if ($row['status'] == 99) {
               echo "<script type='text/javascript' charset='utf-8'>";
