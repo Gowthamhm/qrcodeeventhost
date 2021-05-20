ad636206ee4c6a81e256ceb7c7e0fe50e1845a21
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 16:42:46 +0530

    twilio number display

--- production/send_sms.php
+++ production/send_sms.php
@@ -16,26 +16,40 @@ if (isset($_POST['sendsms'])) {
     $number = [$number];
   }
 
-// Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'AC11111a46dcd23e4a639e77e6088b32c4';
-$auth_token = '7d10e6525a0193bce77b73ffb03a71c6';
+  // Your Account SID and Auth Token from twilio.com/console
+  $account_sid = 'AC11111a46dcd23e4a639e77e6088b32c4';
+  $auth_token = '7d10e6525a0193bce77b73ffb03a71c6';
 // In production, these should be environment variables. E.g.:
 
 // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
 // A Twilio number you own with SMS capabilities
 $twilio_number = "+17204087706";
-$client = new Client($account_sid, $auth_token);
-for($i=0;$i<count($number);$i++){
-  $client->messages->create(
-      // Where to send a text message (your cell phone?)
-      $number[$i],
-      array(
-          'from' => $twilio_number,
-          'body' => $text
-      )
-  );
+ $client = new Client($account_sid, $auth_token);
+ // $client->messages->create(
+ //     // Where to send a text message (your cell phone?)
+ //     '+919483621844',
+ //     array(
+ //         'from' => $twilio_number,
+ //         'body' => 'I sent this message in under 10 minutes!'
+ //     )
+ // );
+for($i=0;$<count($number);$i++){
+  echo $number[$i];
 }
+
+//
+// $client = new Client($account_sid, $auth_token);
+// for($i=0;$i<count($number);$i++){
+//   $client->messages->create(
+//       // Where to send a text message (your cell phone?)
+//       $number[$i],
+//       array(
+//           'from' => $twilio_number,
+//           'body' => $text
+//       )
+//   );
+// }
 }
 
 // $service_plan_id = "78125b9858494c72894913f48031923d";
