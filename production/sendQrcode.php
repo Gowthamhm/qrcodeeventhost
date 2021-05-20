96e362a85250a5b5ca1ab7879f1829c17c641668
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 14:24:49 +0530

    display no of rows

--- production/sendQrcode.php
+++ production/sendQrcode.php
@@ -170,6 +170,7 @@ if (!empty($_SESSION['folder_name'])) {
                                     $sql = "SELECT * FROM `qrcode` where slno='".$qrcodeData[$j]."'";
                                     echo $sql;
                                     $result = $conn->query($sql);
+                                    echo $result->num_rows;
                                     if ($result->num_rows > 0) {
                                       while ($row = $result->fetch_assoc()) {
                                         echo "<tr><td>";
