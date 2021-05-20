81057969b0b67238dfcab1f17815d2f639b434c1
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 16:17:02 +0530

    undo

--- production/sendQrcode.php
+++ production/sendQrcode.php
@@ -125,6 +125,8 @@ if (!empty($_SESSION['folder_name'])) {
       <div class="clearfix"></div>
     </div>
     <div class="x_content">
+        <div class="row">
+            <div class="col-sm-12">
               <div class="card-box table-responsive">
       <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
         <thead>
