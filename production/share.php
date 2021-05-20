bbc0eb18955a8b5a68adb6760899852948978d51
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 21:54:12 +0530

    in qrcode

--- production/share.php
+++ production/share.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
 
 include 'connection.php';
 include 'error.php';
@@ -13,10 +15,14 @@ if (isset($_POST['send'])) {
     $slnos = [$slno];
   }
 
-  $service_plan_id = "78125b9858494c72894913f48031923d";
-  $bearer_token = "63045e8e65ae445b8b65d9f8b7a657cb";
+  // Your Account SID and Auth Token from twilio.com/console
+  $account_sid = 'AC11111a46dcd23e4a639e77e6088b32c4';
+  $auth_token = '70afc0450391fadee1586c848756642b';
+// In production, these should be environment variables. E.g.:
+// $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
-  $send_from = "+447537454577";
+// A Twilio number you own with SMS capabilities
+$twilio_number = "+17204087706";
   // echo "count ".count($slnos);
   for ($i = 0; $i < count($slnos); $i++) {
     $sl = $slnos[$i];
@@ -32,39 +38,65 @@ if (isset($_POST['send'])) {
           $number = "+91" . $row['number'];
           $text = "https://sample-wesite-hosting.online/production" . str_replace(".", '', $row['path']) . "/" . $row['infilename'];
           if (strlen($number) == 13) {
-            $number = ["+91" . $row['number']];
-            $content = [
-              'to' => array_values($number),
-              'from' => $send_from,
-              'body' => $text
-            ];
+            try{
+              $client = new Client($account_sid, $auth_token);
 
-            $data = json_encode($content);
-
-            $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-            curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-            $result = curl_exec($ch);
-            // echo "result".$result;
-            if (curl_errno($ch)) {
-              echo 'Curl error: ' . curl_error($ch);
-            } else {
-              // echo "result".$result;
-              curl_close($ch);
-              $updatestatus =  "UPDATE `qrcode` SET `status`=1 WHERE `slno` = " . $sl;
-              if ($conn->query($updatestatus) === TRUE) {
-?><script type="text/javascript" charset="utf-8">
-                  alert("Message Send Successfully");
-                </script>
-            <?php
-              }
+             // $text = str_replace('<p>',' ',$text);
+             // $text = str_replace('</p>',' ',$text);
+             // $text = str_replace('&nbsp;',' ',$text);
+             // $text = strip_tags($text);
+             // $text = str_replace('&quot','"',$text);
+             // $text = str_replace(';',' ',$text);
+             $client->messages->create(
+                 // Where to send a text message (your cell phone?)
+                 $number,
+                 array(
+                     'from' => $twilio_number,
+                     'body' => $text
+                 )
+             );
+             $updatestatus =  "UPDATE `qrcode` SET `status`=1 WHERE `slno` = " . $sl;
+             if ($conn->query($updatestatus) === TRUE) {
+               ?><script type="text/javascript" charset="utf-8">
+                 alert("Message Send Successfully");
+               </script>
+           <?php
+             }
             }
+            catch (Exception $e){
+              ?><script type="text/javascript" charset="utf-8">
+                // alert($e.get_code());
+                alert("Message not send Succusfully");
+                // window.location.replace('instancesms.php');
+              </script>
+              <?php
+            }
+            // $number = ["+91" . $row['number']];
+            // $content = [
+            //   'to' => array_values($number),
+            //   'from' => $send_from,
+            //   'body' => $text
+            // ];
+            //
+            // $data = json_encode($content);
+            //
+            // $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
+            // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+            // curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
+            // curl_setopt($ch, CURLOPT_POST, true);
+            // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            //
+            // $result = curl_exec($ch);
+            // echo "result".$result;
+            // if (curl_errno($ch)) {
+            //   echo 'Curl error: ' . curl_error($ch);
+            // } else {
+              // echo "result".$result;
+              // curl_close($ch);
+            // }
           } else {
             ?><script type="text/javascript" charset="utf-8">
               alert("message not send Successfully");
