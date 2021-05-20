ad1d57ebe735270286ecaee6833a198c97a2e2d8
Gowthamhm <gowthamhm001@gmail.com>
2021-05-20 21:25:54 +0530

    session

--- production/session.php
+++ production/session.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+// if(!isset($_SESSION['otp'])){
+//   echo '<script type="text/javascript">';
+//   echo 'window.location.href="otp.php";';
+//   echo '</script>';
+// }
 if (!isset($_SESSION['active_user'])) {
   echo '<script type="text/javascript">';
   echo 'window.location.href="logout.php";';
