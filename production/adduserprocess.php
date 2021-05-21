18b8b54018ccb0f057d9004fb9b553ffdb722bd5
Gowthamhm <gowthamhm001@gmail.com>
2021-05-21 12:41:33 +0530

    data

--- production/adduserprocess.php
+++ production/adduserprocess.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
+include 'connection.php';
+include 'error.php';
+
 if (isset($_POST['add'])) {
-  echo $_POST['username'];
-  echo $_POST['email'];
-  echo $_POST['phone'];
-  echo $_POST['role'];
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+  $role ='user';
+  echo $username.'\n'.$email.'\n'.$phone.'\n'.$role;
 }
 ?>
